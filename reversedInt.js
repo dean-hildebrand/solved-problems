a0be66c56eda6339d35259fb6f606cf494aa5f2f
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-12-14 11:55:04 -0500

    Reverse an integer positive or negative

---
+++ reversedInt.js
@@ -0,0 +1,25 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+// function reverseInt(n) {
+//   const reversed = n.toString().split("").reverse().join("");
+
+//   return n < 0 ? parseInt(reversed) * -1 : parseInt(reversed)
+// }
+
+function reverseInt(n) {
+  let result = [];
+  let stringNum = n.toString().split("");
+
+  for (let i = stringNum.length - 1; i >= 0; i--) {
+    result += stringNum[i];
+  }
+  return n < 0 ? parseInt(result) * -1 : parseInt(result);
+}
