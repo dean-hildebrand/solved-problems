9fa71ea02ba25949cee0a159e660cf8780ce84b3
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-12-14 12:30:31 -0500

    max characters with object key and value

---
+++ maxChar.js
@@ -0,0 +1,30 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+  let result = {};
+  let stringDownCase = str.toLowerCase();
+  let max = 0;
+  let maxChar = "";
+
+  for (let char of stringDownCase) {
+    if (!result[char]) {
+      result[char] = 1;
+    } else {
+      result[char]++;
+    }
+  }
+  for (let char in result) {
+    if (result[char] > max) {
+      max = result[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
+
+maxChar("Hello World");
