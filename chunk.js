3fdf794ca44ff4c8e2a97ecb35cd2eb2656fcf83
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-12-16 12:07:57 -0500

    Slice solution and comments

--- chunk.js
+++ chunk.js
@@ -9,18 +9,33 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-    const chunked = [];
+  const chunked = [];
 
-    for (let element of array) {
-        const last = chunked[chunked.length - 1];
+  for (let element of array) {
+    // chunked[chunked.length - 1] is the sub array [1], [1, 2]
+    //checking the length of each subarray to see if you should add to it or start a new one depending on "size" argument
+    const last = chunked[chunked.length - 1];
 
-        if (!last || last.length === size) {
-            chunked.push([element]);
-        } else {
-            last.push(element);
-        }
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else {
+      last.push(element);
     }
-    return chunked
+  }
+  return chunked;
 }
+
+// Slice method for the solution
+
+// function chunk(array, size){
+//     const chunked = [];
+//     let index = 0;
+
+//     while(index < array.length) {
+//         chunked.push(array.slice(index, index + size))
+//         index = index + size
+//     }
+//     return chunked
+// }
 
 module.exports = chunk;
