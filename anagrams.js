eacd2fce0fbccaa553c596913fb9b5f26d80795f
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-12-18 09:41:20 -0500

    Second anagram solution, sort and compare

--- anagrams.js
+++ anagrams.js
@@ -8,31 +8,46 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagram(strA, strB){
-    const aCharMap = buildCharMap(strA);
-    const bCharMap = buildCharMap(strB);
-    
-    //compare number of keys to check length
-    if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-      return false;
+
+  //second solution
+
+    function anagram(strA, strB){
+       return cleanedStr(strA) === cleanedStr(strB);
     }
-  //   //iterate over one charMap to compare values with other
-    for (let char in aCharMap) {
-      if(aCharMap[char] !== bCharMap[char]) {
-        return false;
-      }
+
+    //helper to clean string, remove capitals and punctuation. Then split into array and sort alphabetically, then join back
+    function cleanedStr(str) {
+        return str..replace(/[^\w]/g, "").toLowerCase().split("").sort().join("")
     }
-    return true 
-  }
-  
-  function buildCharMap(str) {
-    const charMap = {};
-    let cleanedStr = str.replace(/[^\w]/g, "").toLowerCase();
-  
-    for (let char of cleanedStr) {
-      charMap[char] = charMap[char] + 1 || 1;
-    }
-    return charMap;
-  }
+
 
 module.exports = anagrams;
+
+//first solution ++++++++++++++
+
+// function anagram(strA, strB){
+//     const aCharMap = buildCharMap(strA);
+//     const bCharMap = buildCharMap(strB);
+    
+//     //compare number of keys to check length
+//     if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//       return false;
+//     }
+//   //   //iterate over one charMap to compare values with other
+//     for (let char in aCharMap) {
+//       if(aCharMap[char] !== bCharMap[char]) {
+//         return false;
+//       }
+//     }
+//     return true 
+//   }
+  
+//   function buildCharMap(str) {
+//     const charMap = {};
+//     let cleanedStr = str.replace(/[^\w]/g, "").toLowerCase();
+  
+//     for (let char of cleanedStr) {
+//       charMap[char] = charMap[char] + 1 || 1;
+//     }
+//     return charMap;
+//   }
