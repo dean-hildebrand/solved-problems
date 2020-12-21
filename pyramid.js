93b171db162d4fcc931ae975c7e1e6768d08442f
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-12-21 09:32:15 -0500

    Recursive pyramid solution

--- pyramid.js
+++ pyramid.js
@@ -14,6 +14,8 @@
 //       ' ### '
 //       '#####'
 
+
+//iterative solution
 function pyramid(n) {
   const midpoint = Math.floor((2 * n - 1) / 2);
 
@@ -31,4 +33,23 @@ function pyramid(n) {
   }
 }
 
-module.exports = pyramid;
+// recursive solution
+
+function pyramid(n, row = 0, level = "") {
+  if (row === n) {
+    return;
+  }
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add += "#";
+  } else {
+    add += " ";
+  }
+  pyramid(n, row, level + add);
+}
+
