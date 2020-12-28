73d8a932069566fe749fdfbbff40b232fed3624c
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-12-28 13:32:17 -0500

    Matrix solution

--- vowels.js
+++ vowels.js
@@ -7,36 +7,41 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-
 //iterative solution
 function vowels(str) {
-  let result = 0
+  let result = 0;
 
-for (let char of str.toLowerCase()){
-  if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
-    result++
+  for (let char of str.toLowerCase()) {
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      result++;
+    }
   }
-}
-return result
+  return result;
 }
 
 //second iterative solution
 function vowels(str) {
-    let result = 0
-    const letters = "aeiou"
-  
-  for (let char of str.toLowerCase()){
+  let result = 0;
+  const letters = ["a", "e", "i", "o", "u"];
+
+  for (let char of str.toLowerCase()) {
     if (letters.includes(char)) {
-      result++
+      result++;
     }
   }
-  return result
-  }
+  return result;
+}
 
-  //regex
-  // g - runs through the whole str and not just the first one
-  //i - takes care of upper/lowercase
-  function vowels(str) {
-      const matches = str.match(/[aeiou]/gi);
-      return matches ? matches.length : 0;
-  }
+//regex
+// g - runs through the whole str and not just the first one
+//i - takes care of upper/lowercase
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
